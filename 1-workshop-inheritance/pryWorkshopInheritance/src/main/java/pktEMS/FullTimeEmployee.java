0c8f25feff35a04671e3ca9133e774c7a7d16b1c
AbdelrahmanCH. <abdelrahman.chehaboddin@utp.ac.pa>
2024-04-13 21:40:10 -0500

    Adding Methods

--- 1-workshop-inheritance/pryWorkshopInheritance/src/main/java/pktEMS/FullTimeEmployee.java
+++ 1-workshop-inheritance/pryWorkshopInheritance/src/main/java/pktEMS/FullTimeEmployee.java
@@ -7,4 +7,31 @@ public class FullTimeEmployee extends Employee {
     public FullTimeEmployee(int id, String firstName, String lastName, String hiringDate, float salary, int workedHours) {
         super(id, firstName, lastName, hiringDate, salary, workedHours);
     }
+
+    public double CalculateSalary() {
+        // Implement salary calculation
+        return this.getSalary() * this.getWorkedHours();
+    }
+    
+    public void requestVacation(int vacationDays) {
+        System.out.println("Vacation request for " + vacationDays + " days in process.");
+        // Implement logic to handle vacation requests (deduct days, send for approval, etc.)
+    }
+    
+    public void registerAttendance() {
+        System.out.println("Welcome, " + this.getName() + ". Registering your presence...");
+        this.setPresentAttendance(this.getPresentAttendance() + 1);
+        System.out.println("Attendance recorded as present.");
+    }
+
+    public void receiveBonus() {
+        double bonusAmount = this.getSalary() * 0.1; // Bonus is 10%
+        System.out.println("Recieve a bonus of $" + bonusAmount + ".");
+        this.setSalary(this.getSalary() + bonusAmount);
+    }
+
 }
+
+
+
+
