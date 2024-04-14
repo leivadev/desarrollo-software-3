70616055e8bdeed37b7b65de5b7eefdcc9fbd8c4
Luislev <luis.leiva@utp.ac.pa>
2024-04-13 21:06:05 -0500

    Add PartTimeEmployee methods

--- 1-workshop-inheritance/pryWorkshopInheritance/src/main/java/pktEMS/PartTimeEmployee.java
+++ 1-workshop-inheritance/pryWorkshopInheritance/src/main/java/pktEMS/PartTimeEmployee.java
@@ -7,4 +7,21 @@ public class PartTimeEmployee extends Employee {
     public PartTimeEmployee(int id, String firstName, String lastName, String hiringDate, float salary, int workedHours) {
         super(id, firstName, lastName, hiringDate, salary, workedHours);
     }
+
+    public float calculateSalary() {
+        return this.salary + this.workedHours;
+    }
+
+    public void requestAbscentPermit() {
+        System.out.println("Abscent permit successfully requested.");
+    }
+
+    public void organizeInventory() {
+        System.out.println("Product inventory organized.");
+    }
+
+    public void attendClients() {
+        System.out.println("Attending clients during work hours...");
+    }
+
 }
