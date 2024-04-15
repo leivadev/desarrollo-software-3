f352502c9cde2981f94bae7a15dd53eb8319b560
AbdelrahmanCH. <abdelrahman.chehaboddin@utp.ac.pa>
2024-04-14 21:50:26 -0500

    repair

--- 1-workshop-inheritance/pryWorkshopInheritance/src/main/java/pktEMS/FullTimeEmployee.java
+++ 1-workshop-inheritance/pryWorkshopInheritance/src/main/java/pktEMS/FullTimeEmployee.java
@@ -26,73 +26,7 @@ public class FullTimeEmployee extends Employee {
         this.setSalary((float) (this.getSalary() + bonusAmount));
     }
 
-    public final class GraphicDesigner extends FullTimeEmployee {
-
-        public GraphicDesigner(int id, String firstName, String lastName, String hiringDate, float salary, int workedHours) {
-          super(id, firstName, lastName, hiringDate, salary, workedHours);
-        }
-      
-        // Method 1: Create a design
-        public void createDesign(String projectName) {
-          System.out.println("Creating a design for project: " + projectName);
-        }
-      
-        // Method 2: Revise a design
-        public void reviseDesign(String projectName, int revisionNumber) {
-          System.out.println("Revising design for project: " + projectName + ", Revision: " + revisionNumber);
-        }
-      
-        // Method 3: Present a design
-        public void presentDesign(String projectName) {
-          System.out.println("Presenting design for project: " + projectName);
-        }
-      }
-
-
-      public final class FinancialConsultant extends FullTimeEmployee {
-
-        public FinancialConsultant(int id, String firstName, String lastName, String hiringDate, float salary, int workedHours) {
-          super(id, firstName, lastName, hiringDate, salary, workedHours);
-        }
-      
-        // Method 1: Conduct a Financial Assessment
-        public void conductFinancialAssessment(String clientName) {
-          System.out.println("Conducting financial assessment for client: " + clientName);
-        }
-      
-        // Method 2: Develop a Financial Plan 
-        public void developFinancialPlan(String clientName) {
-          System.out.println("Developing a financial plan for client: " + clientName);
-        }
-      
-        // Method 3: Recommend Investment Strategies
-        public void recommendInvestmentStrategies(String clientName) {
-          System.out.println("Recommending investment strategies for client: " + clientName);
-        }
-      }
-
-      public final class AdministrativeAssistant extends FullTimeEmployee {
-
-        public AdministrativeAssistant(int id, String firstName, String lastName, String hiringDate, float salary, int workedHours) {
-          super(id, firstName, lastName, hiringDate, salary, workedHours);
-        }
-      
-        // Method 1: Schedule Meetings
-        public void scheduleMeeting(String meetingTopic, String attendees) {
-          System.out.println("Scheduling meeting: " + meetingTopic + ", Attendees: " + attendees);
-        }
-      
-        // Method 2: Prepare Documents
-        public void prepareDocuments(String documentType, String recipient) {
-          System.out.println("Preparing " + documentType + " for " + recipient);
-        }
-      
-        // Method 3: Manage Travel Arrangements
-        public void manageTravelArrangements(String employeeName, String destination) {
-          System.out.println("Managing travel arrangements for " + employeeName + " to " + destination);
-        }
-      }
-
+  }
 }
 
 
--- 1-workshop-inheritance/pryWorkshopInheritance/src/main/java/pktEMS/PartTimeEmployee.java
+++ 1-workshop-inheritance/pryWorkshopInheritance/src/main/java/pktEMS/PartTimeEmployee.java
@@ -110,4 +110,5 @@ public class PartTimeEmployee extends Employee {
         }
       }
 
+      
 }
